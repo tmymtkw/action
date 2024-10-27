b397a3a32a84fecd32d57766c03dec99857ac3cc
tmymtkw <matsukawatomoya0806@gmail.com>
2024-10-27 16:49:58 +0900

    インゲーム用マップと空のc++クラスを追加
    feature/1

---
+++ action/Source/action/Public/Gamemode/GameModeBaseInGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameModeBaseInGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTION_API AGameModeBaseInGame : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
